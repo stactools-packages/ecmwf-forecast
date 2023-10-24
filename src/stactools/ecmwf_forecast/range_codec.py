d1cf197228a78f41709c453f777c5c639d36e6a3
orbaker <107270698+dorbaker@users.noreply.github.com>
2023-10-24 14:59:08 -0400

    Update range_codec.py
    Updated for readilbility.

--- src/stactools/ecmwf_forecast/range_codec.py
+++ src/stactools/ecmwf_forecast/range_codec.py
@@ -17,11 +17,12 @@ class Range(Codec):
     def _get_start_stop_inc(self, array):
         start = array[0]
         stop = array[-1]
-        delta = np.mean(array[1:] - array[0:-1])
-        deltap = np.round(delta,2)
-        if np.abs(delta-deltap) > 1e-10:
-            print("Delta may not be accurate:", delta, deltap)
-        return start, np.round(stop+deltap,2), deltap
+        diffs = array[1:] - array[0:-1]
+        delta = np.mean(diffs)
+        max_diff = np.max(np.abs(delta-diffs))
+        if max_diff > 1e-10:
+            print("Delta may not be accurate:", delta, max_diff)
+        return start, stop+delta, delta
     
     def encode(self,buf):
         info = self._get_start_stop_inc(np.frombuffer(buf,dtype=self.dtype))
@@ -30,4 +31,4 @@ class Range(Codec):
     
     def decode(self,buf):
         new_arr = np.arange(*[i for i in np.frombuffer(buf, dtype=self.dtype)], dtype=self.dtype)
-        return new_arr    
+        return new_arr   
